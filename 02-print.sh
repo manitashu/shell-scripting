11138cbd11e806e6c58a4e7a551f858324ecd108
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-02 00:12:34 +0530

    commented on lines

---
+++ 02-print.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+## Printing something on screen can be done by two commands
+# 1. echo
+# 2. printf
+
+# printf command will have more syntaxes to deal with and however echo is more descriptive and yet does all the things that  printf does.
+
+echo Hello World
