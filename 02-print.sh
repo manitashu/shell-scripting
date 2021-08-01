79d6dbd96c5cb446544a7a04598a2bf5edab3c04
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-02 00:50:52 +0530

    color on lines

--- 02-print.sh
+++ 02-print.sh
@@ -7,3 +7,25 @@
 # printf command will have more syntaxes to deal with and however echo is more descriptive and yet does all the things that  printf does.
 
 echo Hello World
+
+## Syntax for color and new lines : echo -e "MESSAGE\n\t\e"
+# echo -e option do need message to be quoted (either single or double quotes) | (without quotes also works but with more syntaxing)
+
+
+# Print multiple lines (\n), some tab space (/t)
+
+echo -e Hello World, \n\tWelcome
+
+# Enable colors (\e)
+
+#syntax : echo -e "\e[COLmMESSAGE"
+# Colors and their codes are
+# RED     31
+# GREEN   32
+# YELLOW  33
+# BLUE    34
+# MAGENTA 35
+# CYAN    36
+
+echo -e "\e[31mMESSAGE IN RED"
+echo -e "\e[33mMESSAGE IN YELLOW"
