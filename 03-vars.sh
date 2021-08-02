1713cdf0d7ea0d95ecb0d56e61620d28fdb1d0d5
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-02 13:32:43 +0530

    variable examples

--- 03-vars.sh
+++ 03-vars.sh
@@ -2,7 +2,14 @@
 
 A=10
 echo A = $A
+DATE="14-07-21"
+echo Welcome, today date is $DATE
 
 ## Command subs
 N0_OF_USERS=$(who |wc -1)
 echo Number of users = $N0_OF_USERS
+
+## Date with command substitution
+DATE=$(date +%F)
+echo Welcome,today date is $DATE
+
