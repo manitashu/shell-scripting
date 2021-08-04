73f97bccfea6691256bbf160d4d42d5fa9aadce0
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-04 16:41:30 +0530

    functions examples

--- 06-functions.sh
+++ 06-functions.sh
@@ -4,6 +4,14 @@
 
 Sample () {
   echo Hello, I am Sample function
+  echo value of a = ${a}
+  b=200
+  echo First Argument in Functions = $1
 }
+## Main Program
 
 ## Call the function
+a=100
+sample
+echo value of b = ${b}
+echo First Argument in the main program = $1
