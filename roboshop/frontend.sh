49b457e03ec6633fcab46a86842013cca5dd882d
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-08 14:35:10 +0530

    add make file

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+LOG=/tmp/roboshop.log
+
 echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
-yum install nginx -y
+yum install nginx -y >/$LOG
+
 ### 1. Output of commands should not be displayed on screen
 ### 2. Validate the command is successful or not
 
 echo "Enabling Nginx"
-systemctl enable nginx
+systemctl enable nginx >/$LOG
 
 echo "Starting nginx"
-systemctl start nginx
+systemctl start nginx >/$LOG
