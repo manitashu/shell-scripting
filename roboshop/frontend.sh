9bd8a46ec625a9631ddec1e1d32732678225a0df
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-08 13:44:00 +0530

    add make file

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,6 +2,8 @@
 
 echo -e "Installing Nginx\t\t...\t\e[32mdone\e[0m"
 yum install nginx -y
+### 1. Output of commands should not be displayed on screen
+### 2. Validate the command is successful or not
 
 echo "Enabling Nginx"
 systemctl enable nginx
