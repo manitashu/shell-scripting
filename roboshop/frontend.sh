18c242db307d0e3aae4a446ca4bc9e0bfb1739b4
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-11 17:12:31 +0530

    Add Make file

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,15 +3,31 @@
 source common.sh
 
 PRINT "Installing Nginx"
-yum install nginx -y  >>$LOG
+yum install nginx -y  &>>$LOG
+STAT_CHECK $?
+
+PRINT "Download frontend"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+PRINT "Remove Old HtDocs"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
+STAT_CHECK $?
+
+PRINT "Extract Frontend Archive"
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>$LOG
+STAT_CHECK $?
+
+PRINT "Update Roboshop Config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 STAT_CHECK $?
 
 PRINT "Enabling Nginx\t"
-systemctl enable nginx  >>$LOG
+systemctl enable nginx &>>$LOG
 STAT_CHECK $?
 
 PRINT "Starting Nginx\t"
-systemctl start nginx >>$LOG
+systemctl restart nginx &>>$LOG
 STAT_CHECK $?
 
 ### 1. Output of commands should not be displayed on screen
