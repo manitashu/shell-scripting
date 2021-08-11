c574cea4e607f34c73924f2013d339439d193b42
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-11 16:10:31 +0530

    Add Make file

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -27,6 +27,7 @@ PRINT "Starting Nginx"
 systemctl start nginx >>$LOG
 STAT_CHECK $?
 
+
 ### 1. Output of commands should not be displayed on screen
 ### 2. Validate the command is successful or not
 ### 3. Need to validate whether the script running as root user or not.
