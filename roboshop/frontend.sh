4bce3057fe148d5752c51adf25aaac30cbfe25ef
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-11 16:09:58 +0530

    exit states

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,29 +3,29 @@
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
-echo -n -e "Installing Nginx\t\t..."
+STAT_CHECK() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32m done\e[0m"
+  else
+    echo -e "\e[31m fail\e[0m"
+    exit 1
+  fi
+}
+PRINT () {
+  echo -n -e "$1\t\t..."
+}
+
+PRINT "Installing Nginx"
 yum install nginx -y  >>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo fail
-fi
+STAT_CHECK $?
 
-echo -n -e "Enabling Nginx\t\t..."
+PRINT "Enabling Nginx"
 systemctl enable nginx  >>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo fail
-fi
+STAT_CHECK $?
 
-echo -n -e "Starting Nginx\t\t..."
+PRINT "Starting Nginx"
 systemctl start nginx >>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m done\e[0m"
-else
-  echo fail
-fi
+STAT_CHECK $?
 
 ### 1. Output of commands should not be displayed on screen
 ### 2. Validate the command is successful or not
