e8a0c28ddc053b9a1a53e1a7cbe97ba2165a0689
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-13 16:26:29 +0530

    Add Make file

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 
-USER_ID=$(id -u)
-if [ "$USER_ID" -ne 0 ]; then
-  echo -e "\e[31mYou should be root user/sudo user to run this script\e[0m"
-  exit 2
-fi
+source common.sh
 
-LOG=/tmp/roboshop.log
-rm -f $LOG
-
-STAT_CHECK() {
-  if [ "$1" -eq 0 ]; then
-    echo -e "\e[32m done\e[0m"
-  else
-    echo -e "\e[31m fail\e[0m"
-    exit 1
-  fi
-}
-
-PRINT () {
-  echo -n -e "$1\t\t..."
-}
-
-PRINT "Install NodeJS"
+PRINT "Install NodeJS\t\t"
 yum install nodejs make gcc-c++ -y &>>$LOG
 STAT_CHECK $?
 
 PRINT "Add Roboshop Application User"
-useradd roboshop &>>$LOG
+id roboshop &>>$LOG
+if [ $? -ne 0]; then
+  useradd roboshop &>>$LOG
+fi
 STAT_CHECK $?
 
