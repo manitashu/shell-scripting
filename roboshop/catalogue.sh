3e6a309590d3123d02fe2b5d2538fd981fecc3ca
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-13 17:16:29 +0530

    Add Make file

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -35,3 +35,15 @@ if [ $? -ne 0 ]; then
 fi
 STAT_CHECK $?
 
+PRINT "Download Application Code"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+PRINT "Extract Downloaded Code"
+cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install
+STAT_CHECK $?
+
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
