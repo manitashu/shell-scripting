3228af1338130e3cd4215e5f0d79ff665f3b5d7e
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-14 00:23:59 +0530

    Add Make file

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -39,12 +39,16 @@ PRINT "Download Application Code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
 STAT_CHECK $?
 
-PRINT "Extract Downloaded Code"
+PRINT "Extract Downloaded Code\t"
 cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>$LOG && rm -rf catalogue && mv catalogue-main catalogue
 STAT_CHECK $?
 
 PRINT "Install NodeJS Dependencies"
 cd /home/roboshop/catalogue && npm install --unsafe-perm &>>$LOG
+STAT_CHECK $?
+
+PRINT "Fix application Permissions"
+chown roboshop:roboshop /home/roboshop -R &>>$LOG
 STAT_CHECK $?
 
 
