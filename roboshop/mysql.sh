6a9b38c2fff30791ec90338bba6cd47821ca3bec
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-14 16:04:46 +0530

    Add Make file

--- roboshop/mysql.sh
+++ roboshop/mysql.sh
@@ -1,1 +1,71 @@
-sdsasdf
+#!/bin/bash
+
+USER_ID=$(id -u)
+if [ "$USER_ID" -ne 0 ]; then
+  echo -e "\e[31mYou should be root user/sudo user to run this script\e[0m"
+  exit 2
+fi
+
+LOG=/tmp/roboshop.log
+rm -f $LOG
+
+STAT_CHECK() {
+  if [ "$1" -eq 0 ]; then
+    echo -e "\e[32m done\e[0m"
+  else
+    echo -e "\e[31m fail\e[0m"
+    echo -e "\e[33m Check the log file for more details, Log file - $LOG"
+    exit 1
+  fi
+}
+
+PRINT () {
+  echo -e "\n##################\t$1\t###################" &>>$LOG
+  echo -n -e "$1\t\t..."
+}
+
+PRINT "Setup MySQL Repos\t"
+echo '[mysql57-community]
+name=MySQL 5.7 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
+enabled=1
+gpgcheck=0' > /etc/yum.repos.d/mysql.repo
+STAT_CHECK $?
+
+PRINT "Install MySQL\t\t"
+yum install mysql-community-server -y &>>$LOG
+STAT_CHECK $?
+
+PRINT "Start MySQL Service\t"
+systemctl enable mysqld &>>$LOG && systemctl start mysqld &>>$LOG
+STAT_CHECK $?
+
+PRINT "Reset MySQL Root Password"
+DEFAULT_PASSWROD=$(grep 'A temporary password' /var/lod/mysqld.log | awk '{print $NF}')
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';" | mysql -uroot -p$(DEFAULT_PASSWROD) &>>$LOG
+STAT_CHECK $?
+
+Next, We need to change the default root password in order to start using the database service.
+# mysql_secure_installation
+
+You can check the new password working or not using the following command.
+
+# mysql -u root -p
+
+Run the following SQL commands to remove the password policy.
+> uninstall plugin validate_password;
+Setup Needed for Application.
+As per the architecture diagram, MySQL is needed by
+
+Shipping Service
+So we need to load that schema into the database, So those applications will detect them and run accordingly.
+
+To download schema, Use the following command
+
+# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+Load the schema for Services.
+
+# cd /tmp
+# unzip mysql.zip
+# cd mysql-main
+# mysql -uroot -pRoboShop@1 <shipping.sql
