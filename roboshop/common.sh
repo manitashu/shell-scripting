98b23346d804a5c7c84ca35d78d8b659a51c68cd
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-15 01:19:42 +0530

    Add Make file

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -90,3 +90,39 @@ JAVA () {
   PERM_FIX
   SETUP_SYSTEMD
 }
+
+PYTHON3 () {
+  PRINT "Install Python3\t\t"
+  yum install python36 gcc python3-devel -y &>>$LOG
+  STAT_CHECK $?
+
+  ADD_APPLICATION_USER
+  DOWNLOAD_APP_CODE
+
+  PRINT "Install Python3 Dependencies\t"
+  cd /home/roboshop/${COMPONENT} && pip3 install -r requirements.txt &>>$LOG
+  STAT_CHECK $?
+
+  PERM_FIX
+
+
+Download the repo.
+$ cd /home/roboshop
+$ curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
+$ unzip /tmp/payment.zip
+$ mv payment-main payment
+$ cd payment
+Install the dependencies
+# cd /home/roboshop/payment
+# pip3 install -r requirements.txt
+Note: Above command may fail with permission denied, So run as root user
+
+Update the roboshop user and group id in payment.ini file.
+
+Setup the service
+
+# mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
+# systemctl daemon-reload
+# systemctl enable payment
+# systemctl start payment
+}
