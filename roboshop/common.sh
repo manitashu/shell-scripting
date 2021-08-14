a38ee6720e5d4969c1e7c5e39f71fb38665d673e
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-14 15:03:47 +0530

    Add Make file

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -53,7 +53,7 @@ NodeJS () {
   STAT_CHECK $?
 
   PRINT "Setup SystemD Files\t"
-  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "/MONGO_ENDPOINT/mongo.roboshop.internal/" /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "/MONGO_ENDPOINT/mongodb.roboshop.internal/" /home/roboshop/${COMPONENT}/systemd.service && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
   STAT_CHECK $?
 
   PRINT "Start ${COMPONENT} Service\t"
