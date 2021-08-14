b514d6d44f33455c0bfcb3464f91522c512361f6
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-14 17:06:02 +0530

    Add Make file

--- roboshop/mysql.sh
+++ roboshop/mysql.sh
@@ -48,29 +48,18 @@ if [ $? -ne 0 ]; then
 fi
 STAT_CHECK $?
 
+PRINT "Start MySQL Service\t"
+systemctl enable mysqld &>>$LOG && systemctl start mysqld &>>$LOG
+STAT_CHECK $?
 
+PRINT "Uninstall MySQL Password Policy"
+echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1 &>>$LOG
+STAT_CHECK $?
 
-Next, We need to change the default root password in order to start using the database service.
-# mysql_secure_installation
+PRINT "Download Schema\t"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG
+STAT_CHECK $?
 
-You can check the new password working or not using the following command.
-
-# mysql -u root -p
-
-Run the following SQL commands to remove the password policy.
-> uninstall plugin validate_password;
-Setup Needed for Application.
-As per the architecture diagram, MySQL is needed by
-
-Shipping Service
-So we need to load that schema into the database, So those applications will detect them and run accordingly.
-
-To download schema, Use the following command
-
-# curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-Load the schema for Services.
-
-# cd /tmp
-# unzip mysql.zip
-# cd mysql-main
-# mysql -uroot -pRoboShop@1 <shipping.sql
+PRINT "Load Schema\t\t"
+cd /tmp && unzip -o mysql.zip &>>$LOG && cd mysql-main && mysql -uroot -pRoboShop@1 <shipping.sql &>>$LOG
+STAT_CHECK $?
