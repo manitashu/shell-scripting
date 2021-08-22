d2b2dd731e546d3f3e5689dee2c80373520a1017
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-22 16:17:34 +0530

    Add make file

--- roboshop/rabbitmq.sh
+++ roboshop/rabbitmq.sh
@@ -2,22 +2,22 @@
 
 source /home/centos/shell-scripting/roboshop/common.sh
 
-PRINT "Install ERLang"
+PRINT "Install ERLang\t\t"
 yum list installed | grep erlang &>>$LOG
 if [ $? -ne 0 ]; then
   yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG
 fi
 STAT_CHECK $?
 
-PRINT "Setup RabbitMQ repos"
+PRINT "Setup RabbitMQ repos\t"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG
 STAT_CHECK $?
 
-PRINT "Install RabbitMQ server"
+PRINT "Install RabbitMQ server\t"
 yum install rabbitmq-server -y &>>$LOG
 STAT_CHECK $?
 
-PRINT "Start RabbitMQ Service"
+PRINT "Start RabbitMQ Service\t"
 systemctl enable rabbitmq-server &>>$LOG && systemctl start rabbitmq-server &>>$LOG
 STAT_CHECK $?
 
